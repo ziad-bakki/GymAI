4a2a61ce91c691863734cc80955106fcefdcc7c0
ziad-bakki <bakkizia@msu.edu>
2024-12-21 21:23:40 +0300

    removed commented out code in main2.py

--- main2.py
+++ main2.py
@@ -31,18 +31,5 @@ for _ in range(1000):
 env.close()
 
 
-# episodes = 10
-# for episode in range(1, episodes+1):
-#     state = env.reset()
-#     done = False
-#     score = 0
-#     while not done:
-#         action = random.choice([0,1])
-#         state, reward, done, truncated, _ = env.step(action)
-#         score += reward
-#         env.render()
-
-
-#     print(f"Episode {episode}, Score: {score}")
 
     
